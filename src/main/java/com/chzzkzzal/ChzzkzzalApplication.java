b66f2b64a9389a89bb48a43c00fc7299c89f4816
plan11plan <tjtfy7028@gmail.com>
2025-03-04 17:06:30 +0900

    first-commit

---
+++ src/main/java/com/chzzkzzal/ChzzkzzalApplication.java
@@ -0,0 +1,13 @@
+package com.chzzkzzal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChzzkzzalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChzzkzzalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chzzkzzal/ChzzkzzalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chzzkzzal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChzzkzzalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
