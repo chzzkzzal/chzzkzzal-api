b73e1b414cd498321719ac846d49692fefb05a5f
plan11plan <tjtfy7028@gmail.com>
2025-03-21 03:17:38 +0900

    fix: 치지직 유저 정보 api 응답 실패 문제 고침
    - 회원 비번 삭제
    - jwt 유효 시크릿키 변경(yml)
    - 치지직에서 API 응답 보내주는 객체 매핑 안됏던 문제 해결(ObjectMapper 수정)

--- src/main/java/com/chzzkzzal/core/jwt/JwtProvider.java
+++ src/main/java/com/chzzkzzal/core/jwt/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.chzzkzzal.core.jwt;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -8,12 +9,13 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtProvider {
 
 	// 예시용 비밀키 (HMAC). 실제론 환경변수나 키관리시스템에서 주입
-	private static final String SECRET_KEY = "MY_JWT_SECRET_ABC123";
 	private static final long EXPIRATION_MS = 1000 * 60 * 60 * 24; // 1일
 	@Value("${jwt.expiration.access}")
 	private String accessExpirationTime;
@@ -29,10 +31,13 @@ public class JwtProvider {
 		Date expiry = new Date(now.getTime() + EXPIRATION_MS);
 
 		return Jwts.builder()
-			.setSubject(channelId)    // sub: channelId 등
+			.setSubject(channelId)
 			.setIssuedAt(now)
 			.setExpiration(expiry)
-			.signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+			.signWith(
+				Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey)),
+				SignatureAlgorithm.HS256
+			)
 			.compact();
 	}
 
@@ -44,25 +49,25 @@ public class JwtProvider {
 			.setSubject(channelId)
 			.setIssuedAt(new Date(now))
 			.setExpiration(new Date(expiry))
-			.signWith(SignatureAlgorithm.HS256, jwtSecretKey)
+			.signWith(Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS256)
 			.compact();
 	}
 
 	public boolean validateToken(String token) {
 		try {
+			// 바뀐 부분
 			Jwts.parser()
-				.setSigningKey(SECRET_KEY)
+				.setSigningKey(Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8)))
 				.parseClaimsJws(token);
-			return true; // 파싱 성공 시 유효
+			return true;
 		} catch (JwtException e) {
-			// 유효하지 않으면 예외
 			return false;
 		}
 	}
 
 	public String getChannelIdFromToken(String token) {
 		return Jwts.parser()
-			.setSigningKey(SECRET_KEY)
+			.setSigningKey(jwtSecretKey)
 			.parseClaimsJws(token)
 			.getBody()
 			.getSubject(); // sub 필드
