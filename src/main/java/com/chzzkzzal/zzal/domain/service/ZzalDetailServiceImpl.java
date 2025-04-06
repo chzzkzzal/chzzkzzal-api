4f6e002d88591d6abb11356dcb56145fb5614e54
plan11plan <tjtfy7028@gmail.com>
2025-04-06 21:16:10 +0900

    refactor: 조회시 조회 관련 커맨드 기능 추가
    - 조회수 증가
    - 회원 조회 이력 추가
    - 로그 추가

--- src/main/java/com/chzzkzzal/zzal/domain/service/ZzalDetailServiceImpl.java
+++ src/main/java/com/chzzkzzal/zzal/domain/service/ZzalDetailServiceImpl.java
@@ -1,13 +1,18 @@
 package com.chzzkzzal.zzal.domain.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.chzzkzzal.member.domain.Member;
 import com.chzzkzzal.member.domain.MemberLoader;
 import com.chzzkzzal.zzal.domain.dao.LoadZzalPort;
 import com.chzzkzzal.zzal.domain.model.zzal.Zzal;
-import com.chzzkzzal.zzal.domain.service.view.ZzalViewHistoryService;
-import com.chzzkzzal.zzal.domain.service.view.ZzalViewCountService;
+import com.chzzkzzal.myviewhistory.MyViewHistoryService;
+import com.chzzkzzal.zzal_view_log.ZzalViewLogDto;
+import com.chzzkzzal.zzal_view_log.ZzalViewLogService;
+import com.chzzkzzal.zzalhits.domain.ZzalHits;
+import com.chzzkzzal.zzalhits.service.ZzalHitsService;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,18 +24,26 @@ public class ZzalDetailServiceImpl implements ZzalDetailService{
 
 	private final MemberLoader memberLoader;
 	private final LoadZzalPort loadZzalPort;
-	private final ZzalViewCountService zzalViewCountService;
-	private final ZzalViewHistoryService zzalViewHistoryService;
+	private final ZzalHitsService zzalHitsService;
+	private final MyViewHistoryService myViewHistoryService;
+	private final ZzalViewLogService zzalViewLogService;
 
-	public ZzalDetailResponse loadDetail(Long memberId, Long zzalId, HttpServletRequest request){
-		zzalViewCountService.addViewCount(zzalId,request);
-		// memberId가 null이 아닌 경우에만 회원 이력 추가
+	public ZzalDetailResponse getZZal(Long memberId, Long zzalId, HttpServletRequest request){
+		ZzalHits zzalHits = zzalHitsService.addHits(zzalId, request);
+		zzalViewLogService.addViewLog(getZzalViewLogDto(memberId, zzalId, zzalHits));
 		if (memberId != null) {
-			zzalViewHistoryService.addMemberHistory(zzalId, memberId);
+			myViewHistoryService.addMemberViewHistory(zzalId, memberId);
 		}
 		Zzal zzal = loadZzalPort.findById(zzalId).orElseThrow(() -> new EntityNotFoundException("짤이 DB에 없는데용?"));
 		Long uploaderId = zzal.getMember().getId();
 		Member member = memberLoader.loadMember(uploaderId);
 		return ZzalDetailResponse.toResponse(zzal,member);
+	}
+
+	private static ZzalViewLogDto getZzalViewLogDto(Long memberId, Long zzalId, ZzalHits zzalHits) {
+		return new ZzalViewLogDto(
+			zzalId, memberId, zzalHits.getUniqueIdentifier(), zzalHits.getIpAddress(), zzalHits.getUserAgent(),
+			zzalHits.getBrowserType(), zzalHits.getDeviceType(),
+			LocalDateTime.now());
 	}
 }
