33041e03fba91298fbfc7a2ba9ac4ea35e9d46d5
plan11plan <tjtfy7028@gmail.com>
2025-04-10 12:10:41 +0900

    refactor: 시스템 로그 출력 -> Slf4j

--- src/main/java/com/chzzkzzal/core/client/ChzzkAPIService.java
+++ src/main/java/com/chzzkzzal/core/client/ChzzkAPIService.java
@@ -1,7 +1,10 @@
 package com.chzzkzzal.core.client;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.chzzkzzal.member.dto.ChzzkTokenResponse;
 import com.chzzkzzal.member.dto.ChzzkUserResponse;
@@ -13,33 +16,28 @@ import lombok.RequiredArgsConstructor;
 public class ChzzkAPIService {
 	private final ChzzkApiClient chzzkApiClient;
 
-	@Value("${chzzk.oauth.client-id}")
-	private String clientId;
-
-	@Value("${chzzk.oauth.client-secret}")
-	private String clientSecret;
-
 	/**
 	 * 치지직 인증 코드로 액세스 토큰 발급
 	 */
 	public ChzzkTokenResponse getAccessTokenFromChzzk(String code, String state) {
-		return chzzkApiClient.fetchAccessToken(clientId, clientSecret, code, state);
+		return chzzkApiClient.fetchAccessToken(code, state);
 	}
 
 	/**
 	 * 치지직 토큰 폐기
 	 */
 	public String revokeToken(String token, String tokenTypeHint) {
-		return chzzkApiClient.revokeToken(clientId, clientSecret, token, tokenTypeHint);
+		return chzzkApiClient.revokeToken(token, tokenTypeHint);
 	}
 
 	public ChzzkUserResponse getUserInfo(String accessToken) {
 		ChzzkUserResponse userDto = chzzkApiClient.fetchUserInfo(accessToken);
-
-		System.out.println(
-			"[INFO] signUp user channelId=" + userDto.channelId() + ", channelName=" + userDto.channelName()
-		);
 		return userDto;
+	}
+
+	public String getChannelInfos(List<String> channelIds){
+		String[] array = (String[])channelIds.toArray();
+		return chzzkApiClient.fetchChannelInfo(array);
 	}
 
 }
