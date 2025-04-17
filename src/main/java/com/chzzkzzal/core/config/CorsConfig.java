62601a797fb7138c2486974066e422730201880e
plan11plan <tjtfy7028@gmail.com>
2025-04-17 18:50:32 +0900

    refactor: 프론트패턴 허용

--- src/main/java/com/chzzkzzal/core/config/CorsConfig.java
+++ src/main/java/com/chzzkzzal/core/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
-		config.addAllowedOrigin(FRONT_DOMAIN); // 모든 ip에 응답 허용
+		config.addAllowedOriginPattern(FRONT_DOMAIN);
+		// config.addAllowedOrigin(FRONT_DOMAIN); // 모든 ip에 응답 허용
 		config.addAllowedHeader("*"); // 모든 헤더에 응답 허용
 		config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
 		source.registerCorsConfiguration("/**", config); // 모든 경로에 위 설정 적용
