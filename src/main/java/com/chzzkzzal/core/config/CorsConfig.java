ebaa2f08e40c43800326ef28927c084339912912
plan11plan <tjtfy7028@gmail.com>
2025-04-17 16:42:13 +0900

    refactor: 상수명 변경

--- src/main/java/com/chzzkzzal/core/config/CorsConfig.java
+++ src/main/java/com/chzzkzzal/core/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.chzzkzzal.core.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,15 +9,15 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-	@Value("${chzzkzzal.front}")
-	private String FRONT_DOMAIN;
+	// @Value("${chzzkzzal.front}")
+	// private String FRONT_DOMAIN;
 
 	@Bean
 	public CorsFilter corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
-		config.addAllowedOrigin(FRONT_DOMAIN); // 모든 ip에 응답 허용
+		config.addAllowedOrigin("http://3.35.237.38:8080"); // 모든 ip에 응답 허용
 		config.addAllowedHeader("*"); // 모든 헤더에 응답 허용
 		config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
 		source.registerCorsConfiguration("/**", config); // 모든 경로에 위 설정 적용
