cd360adb1969104feaff766ef882def380222dd8
plan11plan <tjtfy7028@gmail.com>
2025-04-17 15:02:59 +0900

    test: 헬스체크 api 추가

---
+++ src/main/java/com/chzzkzzal/core/HealCheckController.java
@@ -0,0 +1,14 @@
+package com.chzzkzzal.core;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealCheckController {
+	@GetMapping
+	public String check() {
+		return "IS GOOD";
+	}
+}
