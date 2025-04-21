d1f20b1cd1323eebd19275b3643e59d8c50b79e9
plan11plan <tjtfy7028@gmail.com>
2025-04-22 02:13:17 +0900

    refactor: GIF 짤 추출기 내부 구조 책임 분리

--- src/main/java/com/chzzkzzal/zzal/domain/model/metadata/GifMetadataExtractor.java
+++ src/main/java/com/chzzkzzal/zzal/domain/model/metadata/GifMetadataExtractor.java
@@ -3,24 +3,24 @@ package com.chzzkzzal.zzal.domain.model.metadata;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Iterator;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.metadata.IIOMetadataNode;
-import javax.imageio.stream.ImageInputStream;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.chzzkzzal.zzal.domain.model.zzal.ZzalType;
-import com.chzzkzzal.zzal.exception.metadata.MetadataExtractionFailedException;
 import com.chzzkzzal.zzal.exception.metadata.MetadataIOException;
 import com.chzzkzzal.zzal.exception.metadata.MetadataUnsupportedFormatException;
 
+import lombok.RequiredArgsConstructor;
+
 @Component
-public class GifMetadataExtractor
-	extends AbstractMetadataExtractor<GifInfo> {
+@RequiredArgsConstructor
+public class GifMetadataExtractor extends AbstractMetadataExtractor<GifInfo> {
+
+	private final GifFrameAnalyzer frameAnalyzer;
+	private final GifDurationCalculator durationCalculator;
 
 	@Override
 	public boolean supports(ZzalType type) {
@@ -29,75 +29,33 @@ public class GifMetadataExtractor
 
 	@Override
 	protected GifInfo parseMetadata(byte[] bytes, MultipartFile file) {
-		int frames = countFrames(bytes);
-		double seconds = calculateTotalSeconds(bytes, frames);
-		BufferedImage img = readCommonImage(bytes);
-		return new GifInfo(
-			file.getSize(), img.getWidth(), img.getHeight(),
-			frames, seconds,
-			file.getContentType(), file.getOriginalFilename()
-		);
-	}
-
-	private ImageInputStream createStream(byte[] bytes) {
 		try {
-			return ImageIO.createImageInputStream(
-				new ByteArrayInputStream(bytes)
+			ImageReader reader = new ImageReader(bytes);
+
+			int frames = frameAnalyzer.countFrames(reader);
+			double seconds = durationCalculator.calculateTotalDuration(reader, frames);
+			BufferedImage firstFrame = readFirstFrame(bytes);
+
+			return new GifInfo(
+				file.getSize(),
+				firstFrame.getWidth(),
+				firstFrame.getHeight(),
+				frames,
+				seconds,
+				file.getContentType(),
+				file.getOriginalFilename()
 			);
 		} catch (IOException e) {
 			throw new MetadataIOException(e);
 		}
 	}
 
-	private int countFrames(byte[] bytes) {
-		try (ImageInputStream iis = createStream(bytes)) {
-			Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName("gif");
-			if (!it.hasNext())
-				throw new MetadataUnsupportedFormatException();
-			ImageReader reader = it.next();
-			reader.setInput(iis);
-			return reader.getNumImages(true);
-		} catch (IOException e) {
-			throw new MetadataIOException(e);
-		}
-	}
-
-	private double calculateTotalSeconds(byte[] bytes, int numFrames) {
-		try (ImageInputStream iis = createStream(bytes)) {
-			Iterator<ImageReader> it = ImageIO.getImageReadersByFormatName("gif");
-			if (!it.hasNext())
-				throw new MetadataUnsupportedFormatException();
-			ImageReader reader = it.next();
-			reader.setInput(iis);
-			int totalDelay = 0;
-			for (int i = 0; i < numFrames; i++) {
-				IIOMetadataNode root = (IIOMetadataNode)
-					reader.getImageMetadata(i)
-						.getAsTree("javax_imageio_gif_image_1.0");
-				IIOMetadataNode gce = (IIOMetadataNode)
-					root.getElementsByTagName("GraphicControlExtension")
-						.item(0);
-				if (gce != null) {
-					try {
-						totalDelay += Integer.parseInt(
-							gce.getAttribute("delayTime")
-						);
-					} catch (NumberFormatException e) {
-						throw new MetadataExtractionFailedException(e);
-					}
-				}
-			}
-			return totalDelay * 10 / 1000.0;
-		} catch (IOException e) {
-			throw new MetadataIOException(e);
-		}
-	}
-
-	private BufferedImage readCommonImage(byte[] bytes) {
+	private BufferedImage readFirstFrame(byte[] bytes) {
 		try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes)) {
 			BufferedImage img = ImageIO.read(bais);
-			if (img == null)
+			if (img == null) {
 				throw new MetadataUnsupportedFormatException();
+			}
 			return img;
 		} catch (IOException e) {
 			throw new MetadataIOException(e);
