df87159a934ee01c1af9591022e07ab2e52e7b88
plan11plan <tjtfy7028@gmail.com>
2025-04-21 12:08:04 +0900

    refactor: 짤 DB 존재 검증 커스텀 예외 적용

--- src/main/java/com/chzzkzzal/zzal/domain/service/ZzalDetailServiceImpl.java
+++ src/main/java/com/chzzkzzal/zzal/domain/service/ZzalDetailServiceImpl.java
@@ -8,8 +8,8 @@ import com.chzzkzzal.zzal.Events;
 import com.chzzkzzal.zzal.ZzalViewedEvent;
 import com.chzzkzzal.zzal.domain.dao.LoadZzalPort;
 import com.chzzkzzal.zzal.domain.model.zzal.Zzal;
+import com.chzzkzzal.zzal.exception.ZzalNotFoundException;
 
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -22,13 +22,11 @@ public class ZzalDetailServiceImpl implements ZzalDetailService {
 
 	public ZzalDetailResponse getZZal(Long zzalId, HttpServletRequest request) {
 
-		// 1.짤 조회 하고
-		Zzal zzal = loadZzalPort.findById(zzalId).orElseThrow(() -> new EntityNotFoundException("짤이 DB에 없는데용?"));
+		Zzal zzal = loadZzalPort.findById(zzalId).orElseThrow(() -> new ZzalNotFoundException());
 		Long uploaderId = zzal.getMember().getId();
 		Member member = memberLoader.loadMember(uploaderId);
-		// 짤 조회했다는 이벤트를 만들어(DTO) 그리고 이벤트 만들었다고 알려야해. raise 야해.
+
 		Events.raise(new ZzalViewedEvent(zzalId, request, member.getId()));
-		//
 		return ZzalDetailResponse.toResponse(zzal, member);
 	}
 
---
+++ src/main/java/com/chzzkzzal/zzal/exception/ZzalExceptionCode.java
@@ -0,0 +1,18 @@
+package com.chzzkzzal.zzal.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.chzzkzzal.core.common.error.ErrorCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ZzalExceptionCode implements ErrorCode {
+	ZZAL_NOT_FOUND(HttpStatus.NOT_FOUND, "ZZAL_NOT_FOUND", "ZZ001");
+
+	private final HttpStatus status;
+	private final String message;
+	private final String code;
+}
